d75ce41269fd9c063a61bf61d1a7b3a657e5e018
Crowdbotics <team@crowdbotics.com>
2024-05-17 17:42:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blendr-48006",
+  url: "https://blendr-48006.botics.co",
+  api: "https://blendr-48006.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
